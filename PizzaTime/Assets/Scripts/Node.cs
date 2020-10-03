1e7d7185c31b8e34a385cde358059e27f6f11fc1
unknown <torresisaac26@gmail.com>
2020-10-02 17:53:31 -0700

    added path finding

---
+++ PizzaTime/Assets/Scripts/Node.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Vector2Int arrayPos;
+    public List<Node> adjNodes = new List<Node>();
+    public Node parentNode = null;
+    public int pathWeight = -1;
+    public int nodeWeight = 1;
+}

f8c59410c48c7f62dd2d9e7ae7c872cf0e192d10
Rocco <roccoestrada@gmail.com>
2020-10-17 00:12:11 -0700

    Now able to generate random delivery locations

--- PizzaTime/Assets/UI/GetDeliveryPositions.cs
+++ PizzaTime/Assets/UI/GetDeliveryPositions.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Net;
 using UnityEngine;
 using UnityEditor;
+using System;
+using System.Diagnostics;
 
 public class GetDeliveryPositions : MonoBehaviour
 {
     int num_deliveries;
+    System.Random random = new System.Random();
     public GameObject deliveryLocation;
     public GameObject LevelControl;
     List<GameObject> buildings = new List<GameObject>();
 
-
     void Start()
     {
-        num_deliveries = Random.Range(0, 3);
         buildings = LevelControl.GetComponent<LevelControl>().buildings;
     }
         
@@ -22,6 +23,7 @@ public class GetDeliveryPositions : MonoBehaviour
     {
         if (collider.gameObject.tag == "Player")
         {
+            num_deliveries = random.Next(1, 3);
             createDeliveryPositions();
         }
 
@@ -29,14 +31,15 @@ public class GetDeliveryPositions : MonoBehaviour
     
     void createDeliveryPositions()
     {
-        
-        
         for (int i = 0; i < num_deliveries; i++)
         {
-            int randomBuilding = Random.Range(0,buildings.Count-1);
+            int randomBuilding = random.Next(0,buildings.Count-1);
             Vector2Int deliveryPos = buildings[randomBuilding].GetComponent<BuildingControl>().deliveryPos;
-            Debug.Log(deliveryPos);
-            GameObject position = Instantiate(deliveryLocation, new Vector3(deliveryPos.x, deliveryPos.y, 0), Quaternion.identity);
+            for(int j = 0; j < buildings.Count; j++)
+            {
+                GameObject position = Instantiate(deliveryLocation, new Vector3(deliveryPos.x*10, 5, deliveryPos.y*10), Quaternion.identity);
+            }
+
         }
     }
 }
